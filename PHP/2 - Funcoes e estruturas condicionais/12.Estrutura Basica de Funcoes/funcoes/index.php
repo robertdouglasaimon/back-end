494210acb620ee16ee1aa6e136e3f5e80b648202
Robert Douglas <robertdouglasaimon@gmail.com>
2025-07-22 10:39:17 -0300

    UPDATE

---
+++ PHP/2 - Funcoes e estruturas condicionais/12.Estrutura Basica de Funcoes/funcoes/index.php
@@ -0,0 +1,29 @@
+<!-- PHP -->
+
+<!-- Funções no PHP 
+Funções são blocos de codigo que podem ser reutilizados. Eles tem como objetivo executar uma tarefa especifica e podem receber ou nao argumentos. Também podem ser chamados dentro de outras funções e sempre retornam um valor.
+
+Exemplo:
+-->
+<?php
+    function saudacao() {
+        return "<h1>Olá Mundo</h1>";
+    }
+
+    $retorno = saudacao();
+    echo $retorno;
+?>
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Estrutura Básica de Funções</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ PHP/2 - Funcoes e estruturas condicionais/13.Funcoes Parametros Opcionais/funcoes/index.php
@@ -0,0 +1,55 @@
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Estrutura Básica de Funções</title>
+</head>
+<body>
+    <h1>Exercicio 1 de PHP</h1>
+</body>
+</html>
+
+<!-- PHP -->
+
+<!-- Funções no PHP 
+Funções são blocos de codigo que podem ser reutilizados. Eles tem como objetivo executar uma tarefa especifica e podem receber ou nao argumentos. Também podem ser chamados dentro de outras funções e sempre retornam um valor.
+
+-->
+<?php
+function verificaIdade ($anoNascimento) {
+    $anoAtual = date("Y");
+    $idade = $anoAtual - $anoNascimento;
+    return $idade;
+}
+
+function portaoDaBalada($anoNascimento, $acompanhado = false) {
+    $idadeParaEntrar = 16;
+    
+    $idade = verificaIdade($anoNascimento);
+    if ($idade >= $idadeParaEntrar) {
+        return 'Pode entrar na balada';
+    } elseif ($idade < $idadeParaEntrar && $acompanhado == true) {
+        return 'Pode entrar acompanhado';
+    } else {  
+        return 'Não pode entrar';
+    }
+}
+
+echo "1994 anos sem acompanhante: ". portaoDaBalada(1994). "<br>";
+
+echo "1999 anos com acompanhante: ". portaoDaBalada(1999, true). "<br>";
+
+echo "2011 anos sem acompanhante: ". portaoDaBalada(2011). "<br>";
+
+echo "2014 anos com acompanhante: ". portaoDaBalada(2014, true). "<br>";
+
+echo "2015 anos sem acompanhante: ". portaoDaBalada(2015, true). "<br>";
+
+
+
+
+
+?>
