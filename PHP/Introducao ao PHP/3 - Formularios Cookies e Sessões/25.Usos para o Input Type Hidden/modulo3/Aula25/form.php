cde40bfa905fb52ce09dcc91dcb6e21ef6e60f91
Robert Douglas <robertdouglasaimon@gmail.com>
2025-08-19 12:12:03 -0300

    Add exemplos para o tipo de entrada oculta e lição CSRF
    - Adicionados arquivos que demonstram o uso do tipo de entrada oculta em formulários, incluindo lógica PHP, funções auxiliares, CSS e JS.
    
    - Também foram incluídos materiais de referência e imagens para a lição sobre CSRF.

---
+++ PHP/Introducao ao PHP/3 - Formularios Cookies e Sessões/25.Usos para o Input Type Hidden/modulo3/Aula25/form.php
@@ -0,0 +1,89 @@
+<?php
+// require_once('functions.php');
+
+$forma_pagamento = ['boleto', 'credito', 'debito'];
+
+$erro = null;
+$sucesso = null;
+
+
+$mensagem = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+    if(!empty($_POST['honeypot'])) {
+        $erro = 'Ops! Robo detectado!';
+    } 
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!--CDN DO BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="src/css/style.css">
+    <title>Formulário</title>
+</head>
+<body>
+    <div class="container mt-5">
+            <h1>Selecione a forma de pagamento</h1>
+
+            <form action="" method="POST">
+
+            <input type="text" value="nome" placeholder="Digite seu nome"> <br>
+            <input type="text" value="email" placeholder="Digite seu email"> <br>
+            <input type="text" value="mensagem" placeholder="Digite sua mensagem"> 
+            
+            <br>
+            <br>
+            
+            <input type="hidden" name="honeypot" value="">
+            <input type="submit" value="Enviar">   
+            </form>
+            
+
+            <!-- Aqui exibe a mensagem -->
+            <br>
+
+            <?= $erro ?>
+            <?= $sucesso ?>
+            <?= $mensagem ?>
+        </div>
+
+</body>
+<script src="src/js/script.js"></script>
+<!--CDN DO JQUERY-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</html>
+
+<?php
+// Função SANITEZE (STRING): Retorna um dado limpo, ou seja, sem tags HTML. Exemplo de uso:
+// $texto = filter_var($texto, FILTER_SANITIZE_STRING);
+
+// Função SANITIZA (EMAIL): Retorna um email limpo. Exemplo de uso:
+// $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+// Função FILTER_VALIDATE_EMAIL: Valida um email. Exemplo de uso:
+// if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//     echo 'Email valido';
+// }
+
+// Função STRLEN: Retorna o tamanho de uma string. Exemplo de uso:
+// if (strlen($texto) > 10) {
+//     echo 'Texto muito longo';
+// }
+
+// Função FILTER_VAR: Valida um dado. Exemplo de uso:
+// if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//     echo 'Email valido';
+// }
+
+
+
+?>
